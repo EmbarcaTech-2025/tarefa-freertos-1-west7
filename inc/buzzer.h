dd23d995e27a121146b79fc1a2d79bc4db2e5cd0
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-06-13 18:59:05 -0300

    remoção do buzzer.c

--- inc/buzzer.h
+++ inc/buzzer.h
@@ -6,9 +6,27 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define BUZZER_PIN 21 
+#define BUZZER_PIN 21
 
-void buzzer_init();
-void beep(uint duration_ms);
+static inline void buzzer_init()
+{
+    gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
+
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+    pwm_config config = pwm_get_default_config();
+
+    pwm_config_set_clkdiv(&config, 1.0f);
+    pwm_init(slice_num, &config, true);
+
+    pwm_set_gpio_level(BUZZER_PIN, 0);
+}
+
+static inline void beep(uint duration_ms)
+{
+    pwm_set_gpio_level(BUZZER_PIN, 1024);
+    vTaskDelay(pdMS_TO_TICKS(duration_ms));
+    pwm_set_gpio_level(BUZZER_PIN, 0);
+    vTaskDelay(pdMS_TO_TICKS(duration_ms));
+}
 
 #endif
